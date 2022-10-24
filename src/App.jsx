7d107400346f9077ba0696cb1e6d606350a97bda
Marcelo Leão <marcelo.lecampos@gmail.com>
2022-10-24 16:21:28 -0300

    🎉 Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='text-lg bg-amber-200'>
+    <h1 className="text-3xl bg-amber-200">
+    Hello world!
+  </h1>
+  </div>
+  )
+}
+
+export default App
